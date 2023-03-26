4d6381152af60542fe07d0f9f51c8fbb0e0748d8
Daniele Lesniowski Zils <lesniowskidaniele@gmail.com>
2023-03-26 11:40:01 -0400

    finished cli lab

--- lib/grade_reports.py
+++ lib/grade_reports.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
-if __name__ == '__main__':
-    pass
+class MyClass:
+    def __init__(self, user_input):
+        self.value = user_input
+
+def my_function(my_object):
+    # returns a final value for the CLI workflow
+
+ if __name__ == '__main__':
+    user_input = input("Enter something here: ")
+    my_object = MyClass(user_input)
+    print(my_function(my_object))
+ 
